122d30bcd81db6741bba39127a107c6f6e01e157
nikhilballal <nikhilballal.94@gmail.com>
2020-12-05 10:06:47 +1100

    first commit

--- src/firestore/index.js
+++ src/firestore/index.js
@@ -1,11 +1,26 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/storage";
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
 
-// const firebaseApp = !firebase.apps.length
-//   ? firebase.initializeApp(firebaseConfig)
-//   : firebase.app();
-// const db = firebaseApp.firestore();
-// const auth = firebaseApp.auth();
-// const storage = firebaseApp.storage();
+//firebase SDK configuration - line 6-14
+var firebaseConfig = {
+  apiKey: 'AIzaSyDX-j3qJtFT1fEajB7dZXQjWaNfGZ9Vctw',
+  authDomain: 'firestore-react-31f4d.firebaseapp.com',
+  projectId: 'firestore-react-31f4d',
+  storageBucket: 'firestore-react-31f4d.appspot.com',
+  messagingSenderId: '1078017007151',
+  appId: '1:1078017007151:web:9d03ac5b36aec04de130da',
+}
+
+const firebaseApp = !firebase.apps.length //initializes it once
+  ? firebase.initializeApp(firebaseConfig) //initializes the firebase app
+  : firebase.app()
+const db = firebaseApp.firestore()
+const auth = firebaseApp.auth()
+const storage = firebaseApp.storage()
+
+//enable google from authentication (in firebase)
+export async function signInWithGoogle() {
+  new firebase.
+}
